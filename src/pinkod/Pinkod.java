dd66f548bf5a8865f0888376c917efa0876eee1d
evingerbono <bono0823@gmail.com>
2023-09-13 13:12:54 +0200

    ellenörzés_javitása

--- src/pinkod/Pinkod.java
+++ src/pinkod/Pinkod.java
@@ -4,43 +4,52 @@ import java.util.Scanner;
 
 public class Pinkod {
 
-    static String pin;
-    static String kod;
+    static int pin;
+    static int belepesiKod;
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        
+        pinProgram();
+    }
 
+    private static void pinProgram() {
         pinBekeres();
         kodbekeres();
-        ellenorzes();
+        ellenorzes(); 
     }
 
     private static void pinBekeres() {
         System.out.print("Kód megadása: ");
-        pin = sc.next();
-        while (pin.length() < 4 || pin.length() > 6) {
+        String kod = sc.next();
+        while (kod.length() < 4 || kod.length() > 6) {
+            System.out.println("Nem megfelelő a formátum, minimum 4 maximum 6 szám legyen!");
             System.out.print("Kód megadása: ");
-            pin = sc.next();
+            kod = sc.next();
         }
         System.out.println("Pin elmentve!");
+        pin=Integer.parseInt(kod);  
     }
 
     private static void kodbekeres() {
         System.out.print("Kérem a belépési kódot: ");
-        kod = sc.next();
+        String kod= sc.next();
+        belepesiKod=Integer.parseInt(kod);
     }
 
     private static void ellenorzes() {
         int db=0;
-        for (int i = 0; i < pin.length(); i++) {
-            if (pin.charAt(i)!=kod.charAt(i)) {
+        for (int i = 1; i < 3; i++) {
+            if (pin!=belepesiKod) {
                 System.out.println("Hibas pin kód!");
                 kodbekeres();
+                db++;
             }
         }
-        if (db<3) {
+        if (db<=2) {
             System.out.println("Rendben, sikeres belépés");
+        }else{
+            System.out.println("Belépés megtagadva!");
         }
 
     }
